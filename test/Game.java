87ccf076fe452ab51e9a7973d413e4b69bb098f8
mohib181 <iam12afi@gmail.com>
2020-12-10 21:52:29 +0600

    time advance and sense kinda done, needs checking

--- test/Game.java
+++ test/Game.java
@@ -5,13 +5,13 @@ import java.util.Random;
 
 public class Game {
     int n;
-    int probBound;
+    double probBound;
     double[][] board;
     Position ghostPosition;
 
     public Game(int boardSize) {
         this.n = boardSize;
-        this.probBound = 80;
+        this.probBound = 0.8;
         this.board = new double[boardSize][boardSize];
         this.initiateBoard();
     }
@@ -23,7 +23,7 @@ public class Game {
             }
         }
 
-        Random random = new Random();
+        Random random = new Random(System.currentTimeMillis());
         ghostPosition = new Position(random.nextInt(n), random.nextInt(n));
     }
 
@@ -38,16 +38,17 @@ public class Game {
     public void printBoard() {
         System.out.println("============================");
 
-        double count = 0;
-        for (double[] doubles : board) {
-            for (int j = 0; j < board.length; j++) {
-                System.out.format("%.4f", doubles[j]);
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.format("%.4f", board[i][j]);
                 System.out.print("  ");
-                count += doubles[j];
+                sum += board[i][j];
             }
             System.out.println();
         }
-        System.out.println("totalProb: " + count);
+
+        System.out.println("totalProb: " + sum);
         System.out.println("Ghost is at " + ghostPosition.toString());
         System.out.println("============================");
     }
@@ -56,107 +57,114 @@ public class Game {
         return board;
     }
 
-    public int getProbBound() {
+    public double getProbBound() {
         return probBound;
     }
 
-    public void setProbBound(int probBound) {
+    public void setProbBound(double probBound) {
         this.probBound = probBound;
+    }
+
+    public ArrayList<Position> getSideNeighbors(int i, int j) {
+        ArrayList<Position> sideNeighbors = new ArrayList<>();
+
+        if(i-1 >= 0) sideNeighbors.add(new Position(i - 1, j));
+        if(i+1 < n) sideNeighbors.add(new Position(i + 1, j));
+        if(j-1 >= 0) sideNeighbors.add(new Position(i, j-1));
+        if(j+1 < n) sideNeighbors.add(new Position(i, j+1));
+
+        return sideNeighbors;
+    }
+
+    public ArrayList<Position> getDiagNeighbors(int i, int j) {
+        ArrayList<Position> diagNeighbors = new ArrayList<>();
+
+        if(i-1 >= 0) {
+            if(j-1 >= 0) diagNeighbors.add(new Position(i-1, j-1));
+            if(j+1 < n) diagNeighbors.add(new Position(i-1, j+1));
+        }
+        if(i+1 < n) {
+            if(j-1 >= 0) diagNeighbors.add(new Position(i+1, j-1));
+            if(j+1 < n) diagNeighbors.add(new Position(i+1, j+1));
+        }
+
+        return diagNeighbors;
+    }
+
+    public void makeGhostMove() {
+        ArrayList<Position> sideNeighbors = getSideNeighbors(ghostPosition.x, ghostPosition.y);
+        ArrayList<Position> diagNeighbors = getDiagNeighbors(ghostPosition.x, ghostPosition.y);
+
+        Random random = new Random(System.currentTimeMillis());
+        int k = random.nextInt(100);
+        if(k < probBound*100) {
+            //choose from sideMove
+            ghostPosition = sideNeighbors.get(random.nextInt(sideNeighbors.size()));
+            //System.out.println("k: "+ k + " side: " + ghostPosition.toString());
+        }
+        else {
+            int t = random.nextInt(diagNeighbors.size()+1);
+            if (t < diagNeighbors.size()) {
+                //choosing diag move
+                ghostPosition = diagNeighbors.get(t);
+                //System.out.println("k: "+ k + " t: "+ t + " diag: " + ghostPosition.toString());
+            }
+            else {
+                //stay
+                //System.out.println("k: "+ k + " t: "+ t + " stay: " + ghostPosition.toString());
+            }
+        }
+
+        //System.out.println("Ghost moved to " + ghostPosition.toString());
     }
 
     public void advanceTime() {
         //calculate all possible transition
         //update the original grid
         //make the ghost move
+        double sum = 0;
         double[][] temp = new double[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                ArrayList<Position> sideMoves = new ArrayList<>();
-                ArrayList<Position> diagMoves = new ArrayList<>();
-                createMoveList(i, j, sideMoves, diagMoves);
+                ArrayList<Position> sideNeighbors = getSideNeighbors(i, j);
+                ArrayList<Position> diagNeighbors = getDiagNeighbors(i, j);
 
-                double sideProb = probBound/(sideMoves.size()*100.0);
-                double diagProb = (100-probBound)/((diagMoves.size()+1)*100.0);
-                double stayProb = diagProb;
+                //System.out.println("(" + i + "," + j + ")");
                 double value = 0;
-
-                for (Position sideMove : sideMoves) {
-                    value += board[sideMove.x][sideMove.y]*sideProb;
+                for (Position sideNeighbor : sideNeighbors) {
+                    double sideProb = probBound/getSideNeighbors(sideNeighbor.x, sideNeighbor.y).size();
+                    value += board[sideNeighbor.x][sideNeighbor.y]*sideProb;
+                    //System.out.println("side: " + sideNeighbor.toString() + " sideProb: " + sideProb + "neighbor: " + board[sideNeighbor.x][sideNeighbor.y]);
                 }
-                for (Position diagMove : diagMoves) {
-                    value += board[diagMove.x][diagMove.y]*diagProb;
+                for (Position diagNeighbor : diagNeighbors) {
+                    double diagProb = (1-probBound)/getDiagNeighbors(diagNeighbor.x, diagNeighbor.y).size();
+                    value += board[diagNeighbor.x][diagNeighbor.y]*diagProb;
+                    //System.out.println("diag: " + diagNeighbor.toString() + " diagProb: " + diagProb + "neighbor: " + board[diagNeighbor.x][diagNeighbor.y]);
                 }
-                value += board[i][j]*stayProb;
+                value += board[i][j]*((1-probBound)/(diagNeighbors.size()+1));
 
                 temp[i][j] = value;
-
-                /*System.out.print("(" + i + "," + j + ") ");
-                System.out.println("SideProb: " + sideProb + " DiagProb: " + diagProb + " StayProb: " + stayProb + " totalProb: " + (sideProb*sideMoves.size()+diagProb*diagMoves.size()+stayProb) + " sideMoveSize: " + sideMoves.size() + " diagMoveSize: " + diagMoves.size());
-                System.out.println(value);*/
+                sum += value;
             }
         }
 
         for (int i = 0; i < n; i++) {
-            System.arraycopy(temp[i], 0, board[i], 0, n);
+            for (int j = 0; j < n; j++) {
+                board[i][j] = (1.0/sum)*temp[i][j];
+            }
         }
 
         System.out.println("advanced Time");
-        printBoard();
         makeGhostMove();
+        printBoard();
     }
-
-    public void createMoveList(int i, int j, ArrayList<Position> sideMoves, ArrayList<Position> diagMoves) {
-        if(i-1 >= 0) {
-            sideMoves.add(new Position(i-1, j));
-            if(j-1 >= 0) diagMoves.add(new Position(i-1, j-1));
-            if(j+1 < n) diagMoves.add(new Position(i-1, j+1));
-        }
-        if(i+1 < n) {
-            sideMoves.add(new Position(i+1, j));
-            if(j-1 >= 0) diagMoves.add(new Position(i+1, j-1));
-            if(j+1 < n) diagMoves.add(new Position(i+1, j+1));
-        }
-        if(j-1 >= 0) sideMoves.add(new Position(i, j-1));
-        if(j+1 < n) sideMoves.add(new Position(i, j+1));
-    }
-
-    public void makeGhostMove() {
-        ArrayList<Position> sideMoves = new ArrayList<>();
-        ArrayList<Position> diagMoves = new ArrayList<>();
-
-        createMoveList(ghostPosition.x, ghostPosition.y, sideMoves, diagMoves);
-
-        Random random = new Random();
-        int k = random.nextInt(100);
-        if(k < probBound) {
-            //choose from sideMove
-            ghostPosition = sideMoves.get(random.nextInt(sideMoves.size()));
-            //System.out.println("side: " + ghostPosition.toString());
-        }
-        else {
-            int t = random.nextInt(diagMoves.size()+1);
-            if (t < diagMoves.size()) {
-                //choosing diag move
-                ghostPosition = diagMoves.get(t);
-                //System.out.println("diag: " + ghostPosition.toString());
-            }
-            else {
-                //stay
-                //System.out.println("stay: " + ghostPosition.toString());
-            }
-        }
-
-        System.out.println("Ghost is at " + ghostPosition.toString());
-    }
-
 
     public int getDistance(int i, int j) {
         return Math.abs(ghostPosition.x-i)+Math.abs(ghostPosition.y-j);
     }
 
     public char getSenseColor(int distance) {
-        //int distanceBound;
         if(distance <= 2) return 'r';
         else if (distance <= 5) return 'y';
         return 'g';
@@ -165,39 +173,28 @@ public class Game {
     public void sense(int x, int y) {
         int distance = getDistance(x, y);
         char color = getSenseColor(distance);
-        System.out.println("manhattan distance: " + distance + " color: " + color);
 
+        double sum = 0;
         double[][] temp = new double[n][n];
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int d = Math.abs(x-i)+Math.abs(y-j);
 
                 int p;
-                /*if(d <= 2 && color == 'r') {
-                    p = 1;
-                }
-                else if (d <= 5 && color == 'y') {
-                    p = 1;
-                }
-                else if (d <= 2*n && color == 'g') {
-                    p = 1;
-                }
-                else {
-                    p = 0;
-                }*/
-
                 if (color == getSenseColor(d)) p = 1;
                 else p = 0;
 
                 temp[i][j] = board[i][j]*p;
+                sum += temp[i][j];
             }
         }
 
         for (int i = 0; i < n; i++) {
-            System.arraycopy(temp[i], 0, board[i], 0, n);
+            for (int j = 0; j < n; j++) {
+                board[i][j] = (1.0/sum)*temp[i][j];
+            }
         }
-        System.out.println("sensed:");
+        System.out.println("sensed: (" + x + "," + y + ") distance: " + distance + " color: " + color);
         printBoard();
     }
 }
